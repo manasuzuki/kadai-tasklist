bbe54752a88e0a3bad8497f3babb72ab83946308
mana <mn.ctgl.ws@gmail.com>
2020-02-03 02:06:05 +0000

    kadai status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
       @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
   end
 
   def new
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-       @task = Task.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -24,11 +24,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+      
   end
 
   def update
-      @task = Task.find(params[:id])
       
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -40,7 +39,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -48,9 +46,12 @@ class TasksController < ApplicationController
   end
   
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
