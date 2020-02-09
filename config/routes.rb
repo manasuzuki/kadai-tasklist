01f2caf2973c4012979340213a7d5a95850f4d29
mana <mn.ctgl.ws@gmail.com>
2020-02-09 02:08:08 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
    root to: 'tasks#index'
    #トップページにアクセス、indexがトップページ
    resources :tasks
   #編集したり削除したりのいろんな動作が可能になる
+   resources :users, only: [:index, :show, :new, :create]
 end
